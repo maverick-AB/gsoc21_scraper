d71a319a906d59c17af4ce75ffe9be4899c60f0d
Avishek <bhartiavi1123@gmail.com>
2021-05-18 21:48:45 +0530

    done

---
+++ gsoc21scrap.py
@@ -0,0 +1,25 @@
+import csv
+import requests
+
+url= 'https://summerofcode.withgoogle.com/api/program/current/project/'
+
+
+with open('gsoc2021.csv','w', encoding='utf-8', newline='') as csv_file:
+    field_names = ['Name','Project', 'Organization']
+    writer = csv.DictWriter(csv_file, fieldnames=field_names)
+    writer.writeheader()
+
+    for i in range(1,66):
+        params = (
+            ('page', str(i)),
+            ('page_size', '20'),
+        )
+        response = requests.get(url=url, params=params).json()
+        data = response.get("results")
+        for ele in data:
+            vals = dict()
+            vals['Name'] = ele.get("student").get("display_name")
+            vals['Project'] = ele.get("title")
+            vals['Organization'] = ele.get('organization').get('name')
+            writer.writerow(vals)
+            
